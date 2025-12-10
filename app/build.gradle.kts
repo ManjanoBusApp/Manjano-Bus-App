92aa066988da52288afd5786d9f040150797e690
Renee <reneegithinji@gmail.com>
2025-12-10 14:56:20 +0300

    Commit and push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,8 +95,8 @@ dependencies {
     implementation("com.google.firebase:firebase-storage-ktx:20.3.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("io.coil-kt:coil-compose:2.7.0")
-    implementation("com.google.android.gms:play-services-maps:18.1.0")
-    implementation("com.google.maps.android:maps-compose:2.14.0")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.maps.android:maps-compose:4.4.1")
 
 }
 
